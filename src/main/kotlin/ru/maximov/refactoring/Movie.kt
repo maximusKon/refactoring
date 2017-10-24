372198800133660c0597c17e330faf8dc26cb6db
konstantin <maximovkl@gmail.com>
2017-10-24 19:39:18 +0300

    методы расчёта стоимости и бонусных баллов перенесены в класс Movie

--- src/main/kotlin/ru/maximov/refactoring/Movie.kt
+++ src/main/kotlin/ru/maximov/refactoring/Movie.kt
@@ -6,4 +6,28 @@ data class Movie(val title:String, var priceCode:Int) {
         const val REGULAR = 0
         const val NEW_RELEASE = 1
     }
+
+    fun getCharge(daysRented: Int): Double {
+        var result = 0.0
+        when (priceCode) {
+            Movie.REGULAR -> {
+                result += 2
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5
+            }
+            Movie.NEW_RELEASE -> result += daysRented * 3
+            Movie.CHILDRENS -> {
+                result += 1.5
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5
+            }
+        }
+        return result
+    }
+
+    fun getFrequentRenterPoints(daysRented: Int): Int =
+            if ((priceCode == Movie.NEW_RELEASE) && daysRented > 1)
+                2
+            else
+                1
 }
--- src/main/kotlin/ru/maximov/refactoring/Rental.kt
+++ src/main/kotlin/ru/maximov/refactoring/Rental.kt
@@ -3,27 +3,9 @@ package ru.maximov.refactoring
 data class Rental(val movie: Movie, private val daysRented: Int){
 
     fun getCharge(): Double {
-        var result = 0.0
-        when (movie.priceCode) {
-            Movie.REGULAR -> {
-                result += 2
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5
-            }
-            Movie.NEW_RELEASE -> result += daysRented * 3
-            Movie.CHILDRENS -> {
-                result += 1.5
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5
-            }
-        }
-        return result
+        return movie.getCharge(daysRented)
     }
 
-    fun getFrequentRenterPoints(): Int =
-        if ((movie.priceCode == Movie.NEW_RELEASE) && daysRented > 1)
-            2
-        else
-            1
+    fun getFrequentRenterPoints(): Int = movie.getFrequentRenterPoints(daysRented)
 
 }
